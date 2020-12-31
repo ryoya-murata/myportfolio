035d3bc0ad249946a9f5fc0be09013e68735554e
ryoya-murata <ryoya.workng@gmail.com>
2020-12-31 16:03:27 +0900

    worksセクションの記述が終了

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+//==================
+// top
+//==================
+
 var tl = new TimelineMax();
 
 tl.fromTo('.top__img-wrap',1,{
@@ -9,3 +13,24 @@ tl.fromTo('.top__img-wrap',1,{
 },{
     x:0,y:0,rotateZ:0,ease:"power2.easeInOut"
 },0.05);
+
+//==================
+// works
+//==================
+
+var mySwiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 2,
+    spaceBetween: 30,
+    initialSlide: 0,
+    centeredSlides : true,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+    }
+});
