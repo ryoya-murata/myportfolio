5f78d3b59c383b6c75fd13d455d60bb9fff8514d
ryoya-murata <ryoya.workng@gmail.com>
2021-01-02 10:50:41 +0900

    ハンバーガーメニューを作成

--- js/script.js
+++ js/script.js
@@ -68,6 +68,30 @@ $(function(){
     })
 })
 
+$(function () {
+    // クリック時の動作
+    $('.hamberger__line-wrap').on('click', function() {
+        var scrollpos;
+
+        // メニューを閉じる
+        if($(this).hasClass('open')) {
+            $(this).removeClass('open');
+            $('.hamberger-menu').removeClass('open');
+            $('.overlay').removeClass('open');
+            $('body').removeClass('fixed');
+
+
+        // メニューを開く
+        } else {
+            $(this).addClass('open');
+            $('.hamberger-menu').addClass('open');
+            $('.overlay').addClass('open');
+            $('body').addClass('fixed');
+        }
+    });
+});
+
+
 
 //==================
 // top
